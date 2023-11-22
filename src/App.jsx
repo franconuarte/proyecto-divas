81bac44554d82bcf5d3f544cb8f11d06387215da
franconuarte <franconuarte0@gmail.com>
2023-11-21 23:01:54 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount'
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 
 function App() {
   return (
-    <header class="header">
-      <div className="App">
+
+
+    <div className="App">
+      <BrowserRouter>
         <NavBar />
-        <ItemListContainer greeting={'Bienvenidos a Divas'} />
-      </div>
-    </header>
+        <Routes>
+          <Route path='/' element={<ItemListContainer />} />
+          <Route path='/category/:categoryId' element={<ItemListContainer />} />
+          <Route path='/item/:itemId' element={<itemDetailContainer />} />
+          <Route path='*' element={<h1>404 NOT FOUND</h1>} />
+        </Routes>
+      </BrowserRouter>
+      <ItemListContainer greeting={'Bienvenidos a Divas'} />
+      <ItemCount intial={1} stock={10} onAdd={(quantity) => console.log('Cantidad agregada', quantity)} />
+      <itemDetailContainer />
+    </div>
+
+
   );
 }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,17 @@
 import cart from './assets/logo.png';
 import CartWidget from '../CardWidget/CartWidget';
 import estilos from './NavBar.css';
+import { NavLink, link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <nav class="navbar">
-            <img class="logo" src={cart} />
-            <div class="anclas">
-                <a class="ancla" href="">¿Quines Somos?</a>
-                <a class="ancla" href="">Nuestro Catalogo</a>
-                <a class="ancla" href="">Contactanos</a>
+            <link to='/'>
+                <img class="logo" src={cart} />
+            </link>
+            <div className="anclas">
+                <NavLink to={'/category/1'} className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>1</NavLink>
+                <NavLink to={'/category/2'} className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>2</NavLink>
             </div>
             <div class="carro">
                 <CartWidget />
